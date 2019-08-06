32c9352e624c7f3d9b483a620df08a81b1b1673d
jvdsandt <jvdsandt@gmail.com>
2019-08-06 13:14:43 +0200

    onClick working

--- src/components/repos.js
+++ src/components/repos.js
@@ -1,24 +1,38 @@
 import React from "react";
 
-function Repos(props) {
-  const repoList = (
-    <div>
-      {props.repos.map(repo => (
-        <div>
-          <a href="#" onClick={() => this.selectRepo({ repoId: repo.id })}>
-            {repo.domain} / {repo.owner} / {repo.name}
-          </a>
-        </div>
-      ))}
-    </div>
-  );
+class Repos extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { selectedRepoId: null };
+  }
 
-  return (
-    <div>
-      <h1>Repos</h1>
-      {repoList}
-    </div>
-  );
+  selectRepo(repoId) {
+    this.setState({
+      selectedRepoId: repoId
+    });
+  }
+
+  render() {
+    const repoList = (
+      <div>
+        {this.props.repos.map(repo => (
+          <div>
+            <a href="#" onClick={e => this.selectRepo(repo.id)}>
+              {repo.domain} / {repo.owner} / {repo.name}
+            </a>
+          </div>
+        ))}
+      </div>
+    );
+
+    return (
+      <div>
+        <h1>Repos</h1>
+        {repoList}
+        <p>Select repo: {this.state.selectedRepoId}</p>
+      </div>
+    );
+  }
 }
 
 export default Repos;
