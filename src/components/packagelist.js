a5d05fbfe45709ee050b9ca0a46d4b3172a7c3ba
jvdsandt <jvdsandt@gmail.com>
2019-08-07 09:19:37 +0200

    enables rest call

--- src/components/packagelist.js
+++ src/components/packagelist.js
@@ -1,31 +1,31 @@
 import React from "react";
 
 class PackageList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.selectPackage = this.selectPackage.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.selectPackage = this.selectPackage.bind(this);
+    }
 
-  selectPackage(pack) {
-    this.props.onSelectionChange(pack);
-  }
+    selectPackage(pack) {
+        this.props.onSelectionChange(pack);
+    }
 
-  render() {
-    return (
-      <div>
-        <h2>Packages</h2>
-        <ul>
-          {this.props.packages.map(each => (
-            <li>
-              <button onClick={e => this.selectPackage(each)}>
-                {each.name}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h2>Packages</h2>
+                <ul>
+                    {this.props.packages.map(each => (
+                        <li>
+                            <button onClick={e => this.selectPackage(each)}>
+                                {each.name}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default PackageList;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import Repos from "./components/repos";
 import CodeBrowser from "./components/codebrowser";
@@ -6,67 +6,38 @@ import CodeBrowser from "./components/codebrowser";
 import "./styles.css";
 
 const headers = new Headers({
-  "Content-Type": "application/json"
+    "Content-Type": "application/json"
 });
 
 class App extends Component {
-  state = {
-    repos: [
-      {
-        id: 74,
-        name: "QCMagritte",
-        owner: "Afibre",
-        domain: "GitHub",
-        url: "https://github.com/Afibre/QCMagritte.git"
-      },
-      {
-        id: 31,
-        name: "Pharo-OS-Windows",
-        owner: "astares",
-        domain: "GitHub",
-        url: "https://github.com/astares/Pharo-OS-Windows.git"
-      },
-      {
-        id: 13,
-        name: "Renraku",
-        owner: "Uko",
-        domain: "GitHub",
-        url: "https://github.com/Uko/Renraku.git"
-      },
-      {
-        id: 28,
-        name: "Teapot",
-        owner: "zeroflag",
-        domain: "GitHub",
-        url: "https://github.com/zeroflag/Teapot.git"
-      }
-    ]
-  };
+    state = {
+        repos: []
+    };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>spider-browser-react</h1>
-        <h2>List of repositories:</h2>
-        <Repos repos={this.state.repos} />
-        <CodeBrowser />
-      </div>
-    );
-  }
-  /**
-  componentDidMount() {
-    fetch("https://www.cloudctrl.com/git/repos", {
-      crossDomain: true,
-      method: "GET",
-      headers
-    })
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ repos: data });
-      });
-  }
-*/
+    render() {
+        return (
+            <div className="App">
+                <h1>spider-browser-react</h1>
+                <h2>List of repositories:</h2>
+                <Repos repos={this.state.repos}/>
+                <CodeBrowser/>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        fetch("https://www.cloudctrl.com/git/repos", {
+            crossDomain: true,
+            method: "GET",
+            headers
+        })
+            .then(res => res.json())
+            .then(data => {
+                this.setState({repos: data});
+            });
+    }
+
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App/>, rootElement);
