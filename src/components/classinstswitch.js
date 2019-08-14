03742995903bb68857bd9f4ff7e290ad86f10a11
jvdsandt <jvdsandt@gmail.com>
2019-08-14 21:38:23 +0200

    Convert class to function

--- src/components/classinstswitch.js
+++ src/components/classinstswitch.js
@@ -1,32 +1,21 @@
 import React from "react";
 
-class ClassInstSwitch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.switch = this.switch.bind(this);
+const ClassInstSwitch = ({ onSwitch, instanceSide }) => {
+  if (instanceSide) {
+    return (
+      <div>
+        <strong>Inst. Side</strong>{" "}
+        <button onClick={() => onSwitch()}>Class Side</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <button onClick={() => onSwitch()}>Inst. Side</button>{" "}
+        <strong>Class Side</strong>
+      </div>
+    );
   }
-
-  switch() {
-    this.props.onSwitch();
-  }
-
-  render() {
-    if (this.props.instanceSide) {
-      return (
-        <div>
-          <strong>Inst. Side</strong>{" "}
-          <button onClick={e => this.switch()}>Class Side</button>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <button onClick={e => this.switch()}>Inst. Side</button>{" "}
-          <strong>Class Side</strong>
-        </div>
-      );
-    }
-  }
-}
+};
 
 export default ClassInstSwitch;
