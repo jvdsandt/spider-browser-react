752fb5a23204fe310ba82a8a50cc2e9781bec062
Jan van de Sandt <jan.van.de.sandt@nn.nl>
2020-01-24 11:55:12 +0100

    working on package info

--- src/components/packageinfo.js
+++ src/components/packageinfo.js
@@ -1,16 +1,113 @@
 import React from "react";
 import {useFetch} from "../utils/useFetch";
 import {formatSha, formatTimestamp} from "../utils/format";
+import {Card, ListGroup} from "react-bootstrap";
 
-const GitCommitLine = ({ commit }) => {
+const GitCommitLine = ({ titlePrefix, commit }) => {
+    return (
+        <React.Fragment>
+            <div className="d-flex w-100 justify-content-between">
+                <h5>{titlePrefix}{formatTimestamp(commit.datetime)} by {commit.authorName}</h5>
+                <small>{formatSha(commit.sha)}</small>
+            </div>
+            <p>{commit.messageLine}</p>
+        </React.Fragment>
+    );
+};
+
+const MCPackageLine = ({ titlePrefix, mcPackage }) => {
+    return (
+        <React.Fragment>
+            <div className="d-flex w-100 justify-content-between">
+                <h5>{titlePrefix}{formatTimestamp(mcPackage.datetime)} by {mcPackage.author}</h5>
+                <small>{formatSha(mcPackage.uuid)}</small>
+            </div>
+            <p>{mcPackage.message}</p>
+        </React.Fragment>
+    );
+};
+
+const GitRepoList = ({ list}) => {
+    return (
+        <Card>
+            <Card.Header><strong>Git Repositories</strong></Card.Header>
+            <ListGroup>
+                {list.map(each => (
+                    <ListGroup.Item>{each.domain} / {each.owner} / {each.name}</ListGroup.Item>
+                ))}
+            </ListGroup>
+        </Card>
+    );
+};
+
+const MCRepoList = ({ list}) => {
+    return (
+        <Card>
+            <Card.Header><strong>Monticello Repositories</strong></Card.Header>
+            <ListGroup>
+                {list.map(each => (
+                    <ListGroup.Item>{each.repoUrl}</ListGroup.Item>
+                ))}
+            </ListGroup>
+        </Card>
+    );
+}
 
 
+const GitAuthorsList = ({ list }) => {
+    return (
+        <React.Fragment>
+            {list.map(each => (
+                <span>{each}, </span>
+            ))}
+        </React.Fragment>
+    );
+};
+
+const MCAuthorsList = ({ list }) => {
+    return (
+        <React.Fragment>
+            {list.map(each => (
+                <span>{each[0]} ({each[1]}), </span>
+            ))}
+        </React.Fragment>
+    );
+};
+
+const PackageGitInfo = ({ data }) => {
 
     return (
-        <p>{formatSha(commit.sha)} {formatTimestamp(commit.datetime)} by {commit.authorName}</p>
+        <React.Fragment>
+            <GitRepoList list={data.gitRepos} />
+            <Card>
+                <Card.Header><strong>Git Commits</strong></Card.Header>
+                <ListGroup>
+                    <ListGroup.Item><GitCommitLine titlePrefix="Oldest Commit: " commit={data.oldestGitCommit} /></ListGroup.Item>
+                    <ListGroup.Item><GitCommitLine titlePrefix="Newest Commit: " commit={data.newestGitCommit}/></ListGroup.Item>
+                    <ListGroup.Item>Authors: <GitAuthorsList list={data.gitAuthors}/></ListGroup.Item>
+                </ListGroup>
+            </Card>
+        </React.Fragment>
     );
+};
 
-}
+const PackageMCInfo = ({ data }) => {
+
+    return (
+        <React.Fragment>
+            <MCRepoList list={data.mcRepos} />
+            <Card>
+                <Card.Header><strong>Monticello Packages</strong></Card.Header>
+                <ListGroup>
+                    <ListGroup.Item><MCPackageLine titlePrefix="Oldest: " mcPackage={data.oldestMCPackage} /></ListGroup.Item>
+                    <ListGroup.Item><MCPackageLine titlePrefix="Newest: " mcPackage={data.newestMCPackage}/></ListGroup.Item>
+                    <ListGroup.Item>Authors: <MCAuthorsList list={data.mcAuthors}/></ListGroup.Item>
+                </ListGroup>
+            </Card>
+        </React.Fragment>
+    );
+};
+
 
 const PackageInfo = ({ match }) => {
 
@@ -28,9 +125,12 @@ const PackageInfo = ({ match }) => {
     return (
         <React.Fragment>
             <h3>Package: {data.name}</h3>
-            <GitCommitLine commit={data.oldestGitCommit} />
-            <GitCommitLine commit={data.newestGitCommit} />
-            <p>{JSON.stringify(data)}</p>
+            {data.oldestGitCommit &&
+                <PackageGitInfo data={data} />
+            }
+            {data.oldestMCPackage &&
+                <PackageMCInfo data={data} />
+            }
         </React.Fragment>
     );
 };
